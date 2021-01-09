54a714fdc6a8da07a57a4839fa65492d8b5acf12
victornwaose <nwaosevictor1234@gmail.com>
2021-01-09 06:55:32 -0800

    Added: New code base

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import useFetchJobs from './compnonets/useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './compnonets/Job';
+import JobsPagination from './compnonets/JobsPagination';
+import SearchForm from './compnonets/SearchForm';
+
+function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs Finder</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing The Page.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })} 
+      {<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} /> }
+    </Container>
+  )
+}
+
+export default App
